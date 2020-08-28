046bbc0b3734536520dfcdf3dcb3c40563376d5d
dusty <dusty@yandex-team.ru>
2020-08-28 20:55:45 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,6 @@
 // TODO: write code here
 console.log('ololo');
 
-const a = undefined;
-
-console.log(a.ololo);
-
 // for demonstration purpose only
 export default function demo(value) {
   return value;
